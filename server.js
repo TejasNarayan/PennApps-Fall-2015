c754348c8cd6326388adbd6e09f7e0e70ece0989
Brendan <brndnmcmns@gmail.com>
2015-09-06 00:22:59 -0400

    minor change

--- server.js
+++ server.js
@@ -226,7 +226,7 @@ app.post('/incoming', function(req, res) {
 	var message = req.body.Body;
 	var from = req.body.From;
 	sys.log('From: ' + from + ', Message: ' + message);
-	var twiml = '<?xml version="1.0" encoding="UTF-8" ?>\n<Response>\n<Sms>Thanks for your text, we\'ll be in touch.</Sms>\n</Response>';
+	var twiml = '<?xml version="1.0" encoding="UTF-8" ?>\n<Response>\n<Sms>Thanks for your text, we\'ll be in touch.' + message + '</Sms>\n</Response>';
 	res.send(twiml, {'Content-Type':'text/xml'}, 200);
 });
 
